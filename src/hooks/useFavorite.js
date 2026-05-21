0ac449ff6209bc7ecf4fa93c6c9ddda7e1469eda
SandraGEnglund <sandra.g.englund@gmail.com>
2026-05-21 09:55:16 +0200

    Updated with check if there are any initial favorites from the logged in user.

--- src/hooks/useFavorite.js
+++ src/hooks/useFavorite.js
@@ -1,33 +1,60 @@
 import { useState } from "react";
 import { favoritesService } from "../services/favoritesService";
-
-const [isFavorited, setIsFavorited] = useState(false);
-const [isLoading, setIsLoading] = useState(false);
-const [showMessage, setShowMessage] = useState(false);
-
-const toggleFavorites = async () => {
-    if (!userId) return;
-
-    try {
-        setIsLoading(true);
-
-        await (isFavorited
-            ? favoritesService.removeFavorite(userId, recipeId)
-            : favoritesService.addFavorite(userId, recipeId));
-
-        setIsFavorited(!isFavorited)
-        setShowMessage(true);
-        setTimeout(() => setShowMessage(false), 4000);
-
-    } catch (error) {
-        console.error("Couldn't update favorite status", error)
-    } finally {
-        setIsLoading(false);
-    }
-}
+import { useAuth } from "../context/AuthContext";
 
 
+export const useFavorite = (recipeId) => {
+    const userId
+
+    const [isFavorited, setIsFavorited] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [showMessage, setShowMessage] = useState(false);
+
+    useEffect(() => {
+        const checkFavoriteStatus = async () => {
+            if (!userId || !recipeId) return;
+
+            try {
+                const userFavorites = await favoritesService.getFavorites(userId);
+
+                setIsFavorited(userFavorites.includes(recipeId));
+            } catch (error) {
+                console.error("Couldn't check initial favorite status", error)
+            }
+        };
+        checkInitialFavoriteStatus();
+    }, [userId, recipeId]);
 
 
-//  ** Detta tipsar AI om att det kan användas.
-// className={isFavorited ? styles.heartFull : styles.heartEmpty}
+    const toggleFavorites = async () => {
+        if (!userId) return;
+
+        try {
+            setIsLoading(true);
+
+            await (isFavorited
+                ? favoritesService.removeFavorite(userId, recipeId)
+                : favoritesService.addFavorite(userId, recipeId));
+
+            setIsFavorited(!isFavorited)
+            setShowMessage(true);
+            setTimeout(() => setShowMessage(false), 4000);
+
+        } catch (error) {
+            console.error("Couldn't update favorite status", error)
+        } finally {
+            setIsLoading(false);
+        };
+
+        return {
+            isFavorited,
+            isLoading,
+            showMessage,
+            toggleGavorites
+        }
+    };
+};
+
+
+
+
