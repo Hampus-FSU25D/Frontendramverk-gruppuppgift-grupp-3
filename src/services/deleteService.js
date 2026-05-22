fb2dd4337a23e52cb31cf1720f9dc0ddf45eceec
SandraGEnglund <sandra.g.englund@gmail.com>
2026-05-22 18:47:40 +0200

    Created deleteService where the logic for the deletebutton resides.

---
+++ src/services/deleteService.js
@@ -0,0 +1,21 @@
+export const recipesService = {
+  /**
+   * Will delete one specific recipe only if the userId and the created_by is matching. **
+   * @param {string} recipeId 
+   * @returns {Promise<boolean>}
+   */
+  deleteRecipe: async (recipeId) => {
+    try {
+      const { error } = await supabase
+        .from('recipes')
+        .delete()
+        .eq('id', recipeId);
+
+      if (error) throw error;
+      return true;
+    } catch (error) {
+      console.error('Fel vid borttagning av recept:', error.message);
+      throw error;
+    }
+  }
+};
